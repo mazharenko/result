78e3fcbef9c4cc00352cd9951b84dddac9112ebf
mazharenko <mazharenko.a@yandex.ru>
2022-06-29 09:45:00 +0500

    DisplayPipe extension

---
+++ src/result.Interactive/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace System;
+
+[PublicAPI]
+public static class Extensions
+{
+	public static T DisplayPipe<T>(this T o)
+	{
+		o.Display();
+		return o;
+	}
+}
