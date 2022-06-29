67bc103eb47dc098ee36524c2460fd5516fdaa2c
mazharenko <mazharenko.a@yandex.ru>
2022-06-29 16:18:03 +0500

    functor

---
+++ src/result/Extensions/FunctorExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace mazharenko.result;
+
+[PublicAPI]
+public static class FunctorExtensions
+{
+	[MustUseReturnValue]
+	public static Result<TOut, TFailure> Map<T, TOut, TFailure>(this Result<T, TFailure> source,
+		Func<T, TOut> function)
+	{
+		return source.Match(
+			value => Result<TOut, TFailure>.Success(function(value)!),
+			error => Result<TOut, TFailure>.Failure(error!)
+		);
+	}
+
+	[MustUseReturnValue]
+	public static async Task<Result<TOut, TFailure>> MapAsync<T, TOut, TFailure>(this Result<T, TFailure> source,
+		Func<T, Task<TOut>> function)
+	{
+		return await source.MatchAsync(
+				   async value => Result<TOut, TFailure>.Success((await function(value).ConfigureAwait(false))!),
+				   error => Task.FromResult(Result<TOut, TFailure>.Failure(error!))
+			   ).ConfigureAwait(false);
+	}
+
+	[MustUseReturnValue]
+	public static Result<T, TFailure2> OrMap<T, TFailure, TFailure2>(this Result<T, TFailure> source,
+		Func<TFailure, TFailure2> function)
+	{
+		return source.Match(
+			value => Result<T, TFailure2>.Success(value!),
+			error => Result<T, TFailure2>.Failure(function(error)!)
+		);
+	}
+
+	[MustUseReturnValue]
+	public static async Task<Result<T, TFailure2>> OrMapAsync<T, TFailure, TFailure2>(this Result<T, TFailure> source,
+		Func<TFailure, Task<TFailure2>> function)
+	{
+		return await source.MatchAsync(
+				   value =>  Task.FromResult(Result<T, TFailure2>.Success(value!)),
+				   async error => Result<T, TFailure2>.Failure((await function(error).ConfigureAwait(false))!)
+			   ).ConfigureAwait(false);
+	}
+}
--- src/result/Result.cs
+++ src/result/Result.cs
@@ -7,18 +7,22 @@ namespace mazharenko.result;
 [PublicAPI]
 public static class Result
 {
+	[MustUseReturnValue]
 	public static Result<T, TFailure> From<T, TFailure>(bool successCondition, [DisallowNull] T success, [DisallowNull] TFailure failure)
 	{
 		return successCondition ? success : failure;
 	}
 
+	[MustUseReturnValue]
 	public static Result<T, TFailure> From<T, TFailure>(bool successCondition, Func<T> successFactory,
 		Func<TFailure> failureFactory)
 	{
 		return successCondition ? successFactory()! : failureFactory()!;
 	}
 
+	[Pure]
 	public static SuccessResult<T> Success<T>([DisallowNull] T success) => new(success);
 
+	[Pure]
 	public static FailureResult<TFailure> Failure<TFailure>([DisallowNull]TFailure failure) => new(failure);
 }
