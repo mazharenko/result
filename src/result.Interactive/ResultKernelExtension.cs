17290cf97571da34acc180c16eff38a39231f99f
mazharenko <mazharenko.a@yandex.ru>
2022-07-01 01:52:23 +0500

    zip docs

--- src/result.Interactive/ResultKernelExtension.cs
+++ src/result.Interactive/ResultKernelExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.DotNet.Interactive;
@@ -7,31 +8,54 @@ using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
 
 namespace mazharenko.result.Interactive;
 
-internal static class ResultExtensions
+
+[UsedImplicitly]
+public class ResultKernelExtension : IKernelExtension
 {
-	public static string FormatResult<T, TFailure>(this Result<T, TFailure> result)
+	private static PocketView FormatResult<T, TFailure>(Result<T, TFailure> result)
 	{
 		var typeHash = result.Match(typeof(T).GetHashCode(), typeof(TFailure).GetHashCode());
 		return (
 				   (span[style: $"border: 4px solid {result.Match("green", "red")}; " +
 								$"background-color: hsla({typeHash % 360}, 100%, 60%, 0.2); " +
+								"white-space: pre-line;" +
 								"padding: 10px; display: inline-block"](
-						   result.Match<object?>(value => value, error => error)
+						   (result.Match<object?>(value => value, error => error))
 					   ))
-			   ).ToString();
+			   );
 	}
-}
 
-[UsedImplicitly]
-public class ResultKernelExtension : IKernelExtension
-{
+	private static PocketView FormatErrors<T>(IEnumerable<Errors<T>> errors)
+	{
+		return ul[style: "padding-left: 20pt; list-style-type: none"](
+			errors.Select(error => 
+				li(
+					span(error.Root),
+					FormatErrors(error.InnerFailures)
+				)
+			)
+		);
+	}
+	
+	private static PocketView FormatErrors<T>(Errors<T> errors)
+	{
+		return FormatErrors(new[] { errors });
+	}
+	
 	public Task OnLoadAsync(Kernel kernel)
 	{
 		Formatter.Register(
 			type: typeof(Result<,>),
 			formatter: (obj, writer) =>
 			{
-				writer.WriteLine(ResultExtensions.FormatResult((dynamic)obj));
+				writer.WriteLine(FormatResult((dynamic)obj).ToString());
+			}, HtmlFormatter.MimeType);
+
+		Formatter.Register(
+			type: typeof(Errors<>),
+			formatter: (obj, writer) =>
+			{
+				writer.WriteLine(FormatErrors((dynamic)obj).ToString());
 			}, HtmlFormatter.MimeType);
 
 		return Task.CompletedTask;
